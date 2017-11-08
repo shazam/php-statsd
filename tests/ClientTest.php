f34844b39bb24adaf10ea637aa29f8942d2a8c5a
Gabriel Caruso <carusogabriel34@gmail.com>
2017-11-08 08:32:15 -0200

    Use PHPUnit\Framework\TestCase instead of PHPUnit_Framework_TestCase

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -3,14 +3,14 @@
 /**
  * Copyright 2014 Shazam Entertainment Limited
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
  * file except in compliance with the License.
  *
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under 
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * @package Statsd\Tests
@@ -22,12 +22,13 @@ namespace Statsd\Tests;
 use Statsd\Client;
 use Statsd\Client\Configuration;
 use Exception;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @package Statsd\Tests
  */
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+class ClientTest extends TestCase
 {
     public function providerWrongStat()
     {
